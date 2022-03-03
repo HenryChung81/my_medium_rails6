9c7613ec0745db493810276d6a8bf55e6510b643
Henry Chung <chungjenhao81@gmail.com>
2022-03-03 17:59:06 +0800

    feat: friendly & babosa gem

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,7 @@
 class Story < ApplicationRecord
+  extend FriendlyId
+  friendly_id :slug_candidate, use: :slugged
+
   include AASM
   belongs_to :user
   validates :title, presence: true
@@ -23,4 +26,15 @@ class Story < ApplicationRecord
 
   end
 
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
+  private
+  def slug_candidate
+    [
+      :title,
+      [:title, SecureRandom.hex[0, 8]]
+    ]
+  end
 end
