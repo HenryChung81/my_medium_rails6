dc5c066a39c92c449655ff5d9cedc185785fa40b
Henry Chung <chungjenhao81@gmail.com>
2022-03-11 15:35:16 +0800

    feat: add page index template

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,15 @@
 class PagesController < ApplicationController
 
   def index
+    @stories = Story.all.order(created_at: :desc).includes(:user)
+
+  end
+
+  def show
+    
+  end
+
+  def user
     
   end
 end
