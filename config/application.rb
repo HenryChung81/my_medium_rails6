926b24dcb215591ba9c85dbbe724d9a3ad292a0b
Henry Chung <chungjenhao81@gmail.com>
2022-03-23 17:31:09 +0800

    refactor: config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module MyMedium
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.time_zone = 'Asia/Taipei'
+    config.assets.initialize_on_precompile = false
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
